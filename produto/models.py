e1852f7a2e88ea84b87e3d694081e39c672eec58
Kevin <kevin.dias.20160@gmail.com>
2022-01-11 20:04:50 -0300

    feat: create models Variacao and Produto, config img resize and create admin.py

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+from PIL import Image
+from django.conf import settings
+import os
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/',
+        blank=True,
+        null=True
+    )
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        max_length=1, 
+        default='V',
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pill = Image.open(img_full_path)
+        original_width, original_height = img_pill.size
+
+        if original_width <= new_width:
+            img_pill.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+        new_img = img_pill.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    def __str__(self):
+        return self.nome
+
+
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
