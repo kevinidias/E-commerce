f9a2d73ab060c3299a0d82eb06ff6afe01a4fe14
Kevin <kevin.dias.20160@gmail.com>
2022-03-29 15:45:58 -0300

    feat: create detail view and detail.html

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 
@@ -10,8 +11,11 @@ class ListaProdutos(ListView):
     paginate_by = 10
 
 
-class DetalheProduto(View):
-    pass
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AddCarrinho(View):
