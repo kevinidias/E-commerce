a15022b31ef6bcede435d78f4e4615ab62081f04
Kevin <kevin.dias.20160@gmail.com>
2022-03-31 17:43:02 -0300

    feat: create view add car

--- produto/views.py
+++ produto/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
+from django.contrib import messages
 from . import models
 
 class ListaProdutos(ListView):
@@ -19,7 +21,35 @@ class DetalheProduto(DetailView):
 
 
 class AddCarrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        http_referer = self.request.META.get( 
+            'HTTP_REFERER',
+            reverse('produto:lista')
+            
+            )  #URL ANTERIOR
+        variacao_id = self.request.GET.get('vid')
+        
+        if not variacao_id:
+            messages.error(
+                self.request,
+                'Produto não existe'
+            )
+            return redirect(http_referer)
+
+        variacao = get_object_or_404(models.Variacao, id=variacao_id)
+
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+
+        carrinho = self.request.session['carrinho']
+        
+        if variacao_id in carrinho:
+            pass
+        else:
+            pass
+
+        return HttpResponse(f'{variacao.produto} {variacao.nome}')
 
 
 class DeleteCarrinho(View):
