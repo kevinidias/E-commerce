7d6b71e6060dc983dbac4d57d358025973518299
Kevin <kevin.dias.20160@gmail.com>
2022-04-11 11:39:12 -0300

    feat: validation users

--- perfil/forms.py
+++ perfil/forms.py
@@ -49,6 +49,7 @@ class UserForm(forms.ModelForm):
         error_msg_email_exists = 'E-mail já existe'
         error_msg_password_match = 'As duas senhas não conferem'
         error_msg_password_short = 'Sua senha precisa de pelo menos 6 caracteres'
+        error_msg_required_field = 'Este campo é obrigatório.'
 
         if self.usuario:
             if usuario_db:
@@ -68,7 +69,24 @@ class UserForm(forms.ModelForm):
                     validation_error_msgs['password'] = error_msg_password_short
 
         else:
-            ...
+            if usuario_db:
+                validation_error_msgs['username'] = error_msg_user_exists
+
+            if email_db:
+                validation_error_msgs['email'] = error_msg_email_exists
+
+            if not password_data:
+                validation_error_msgs['password'] = error_msg_required_field
+            
+            if not password_data2:
+                validation_error_msgs['password2'] = error_msg_required_field
+
+            if password_data != password_data2:
+                validation_error_msgs['password'] = error_msg_password_match
+                validation_error_msgs['password2'] = error_msg_password_match
+
+            if len(password_data) < 6:
+                validation_error_msgs['password'] = error_msg_password_short
             
         if validation_error_msgs:
             raise(forms.ValidationError(validation_error_msgs))
--- perfil/views.py
+++ perfil/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views import View
+from django.contrib.auth.models import User
+import copy
 
 from . import models
 from . import forms
@@ -12,7 +14,13 @@ class BasePerfil(View):
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
 
+        self.carrinho = copy.deepcopy(self.request.session.get('carrinho', {}))
+
+        self.perfil = None
+        
         if self.request.user.is_authenticated:
+            self.perfil = models.Perfil.objects.filter(usuario=self.request.user).first()
+
             self.contexto = {
                 'userform': forms.UserForm(
                     data=self.request.POST or None, 
@@ -33,6 +41,9 @@ class BasePerfil(View):
                 )
             }
 
+        self.userform = self.contexto['userform']
+        self.perfilform = self.contexto['perfilform']
+
         self.renderizar = render(self.request, self.template_name, self.contexto)
 
     def get(self, *args, **kwargs):
@@ -40,6 +51,36 @@ class BasePerfil(View):
 
 class Criar(BasePerfil):
     def post(self, *args, **kwargs):
+        if not self.userform.is_valid() or not self.perfil.is_valid():
+            return self.renderizar
+
+        username = self.userform.cleaned_data.get('username')
+        password = self.userform.cleaned_data.get('password')
+        email = self.userform.cleaned_data.get('email')
+        first_name = self.user.cleaned_data.get('first_name')
+        last_name = self.user.cleaned_data.get('last_name')
+
+        if self.request.user.is_authenticated:
+            usuario = get_object_or_404(User, username=self.request.user.username)
+            usuario.username = username
+            
+            if password:
+                usuario.set_password(password)
+            
+            usuario.email = email
+            usuario.first_name = first_name
+            usuario.last_name = last_name
+        else:
+            usuario = self.userform.save(commit=False) #não salva na base de dados
+            usuario.set_password(password)
+            usuario.save()
+
+            perfil = self.perfilform.save(commit=False)
+            perfil.usuario = usuario
+            perfil.save()
+
+        self.request.session['carrinho'] = self.carrinho
+        self.request.session.save()
         return self.renderizar
 
 
